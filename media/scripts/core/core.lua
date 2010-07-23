94a33dbe3a49285976c2130a1a380daa0b174cb2
thaler.jonathan <thaler.jonathan@bb54a4e1-d722-1b89-d25c-b3a6cc25c7a0>
2010-07-23 18:48:56 +0000

    first calls to lua working :)

---
+++ media/scripts/core/core.lua
@@ -0,0 +1,35 @@
+print ( "loaded core.lua" );
+
+function onStartup()
+	print( "lua got called: onStartup" );
+end
+
+function postStartup()
+	print( "lua got called: postStartup" )
+end
+
+
+
+function onShutdown()
+	print( "lua got called: onShutdown" )
+end
+
+
+
+function onSubSystemLoad()
+	print( "lua got called: onSubSystemLoad" )
+end
+
+function postSubSystemLoad()
+	print( "lua got called: postSubSystemLoad" )
+end
+
+
+
+function beginFrame( factor )
+	print( "lua got called: beginFrame" )
+end
+
+function endFrame()
+	print( "lua got called: endFrame" )
+end
