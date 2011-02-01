e1abb76972d77c1a2c8c8a8258c5a1e259e9bd23
thaler.jonathan <thaler.jonathan@bb54a4e1-d722-1b89-d25c-b3a6cc25c7a0>
2011-02-01 23:22:25 +0000

    further refactorings. uniformblocks seem messy

--- src/SubSystems/Graphics/ZazenGraphics/Geometry/GeomMesh.cpp
+++ src/SubSystems/Graphics/ZazenGraphics/Geometry/GeomMesh.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 #define BUFFER_OFFSET( i ) ( ( char* ) NULL + ( i ) )
 
-GeomMesh::GeomMesh( int faceCount, int vertexCount, VertexData* data, int* indexBufer )
+GeomMesh::GeomMesh( int faceCount, int vertexCount, VertexData* data, GLuint* indices )
 	: faceCount(faceCount),
 	  vertexCount( vertexCount )
 {
@@ -14,22 +14,22 @@ GeomMesh::GeomMesh( int faceCount, int vertexCount, VertexData* data, int* index
 	this->indexVBO = 0;
 
 	this->data = data;
-	this->indexBuffer = indexBuffer;
+	this->indexBuffer = indices;
 }
 
 GeomMesh::~GeomMesh()
 {
 	if (this->dataVBO )
-	{
 		glDeleteBuffers( 1, &this->dataVBO );
+
+	if (this->indexVBO )
 		glDeleteBuffers( 1, &this->indexVBO );
 
-		if ( this->data )
-			delete this->data;
+	if ( this->data )
+		delete this->data;
 
-		if ( this->indexBuffer )
-			delete this->indexBuffer;
-	}
+	if ( this->indexBuffer )
+		delete this->indexBuffer;
 }
 
 bool
@@ -42,6 +42,7 @@ GeomMesh::render()
 	// lazy loading
 	if ( 0 == this->dataVBO )
 	{
+		// generate and setup data vbo
 		glGenBuffers( 1, &this->dataVBO );
 		status = glGetError();
 		if ( GL_NO_ERROR != status )
@@ -66,38 +67,6 @@ GeomMesh::render()
 			return false;
 		}
 
-		glEnableVertexAttribArray(0);
-		status = glGetError();
-		if ( GL_NO_ERROR != status )
-		{
-			cout << "GeomMesh::render: glEnableVertexAttribArray(0) failed: " << gluErrorString( status )  << endl;
-			return false;
-		}
-
-		glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, sizeof( VertexData ), BUFFER_OFFSET( 0 ) );
-		status = glGetError();
-		if ( GL_NO_ERROR != status )
-		{
-			cout << "GeomMesh::render: glVertexAttribPointer(0) failed: " << gluErrorString( status )  << endl;
-			return false;
-		}
-
-		glEnableVertexAttribArray(1);
-		status = glGetError();
-		if ( GL_NO_ERROR != status )
-		{
-			cout << "GeomMesh::render: glEnableVertexAttribArray(1) failed: " << gluErrorString( status )  << endl;
-			return false;
-		}
-
-		glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, sizeof( VertexData ), BUFFER_OFFSET( 12 ) );
-		status = glGetError();
-		if ( GL_NO_ERROR != status )
-		{
-			cout << "GeomMesh::render: glVertexAttribPointer(1) failed: " << gluErrorString( status )  << endl;
-			return false;
-		}
-
 		glBindBuffer( GL_ARRAY_BUFFER, 0 );
 		status = glGetError();
 		if ( GL_NO_ERROR != status )
@@ -106,6 +75,7 @@ GeomMesh::render()
 			return false;
 		}
 
+		// generate and setup index vbo
 		glGenBuffers( 1, &this->indexVBO );
 		status = glGetError();
 		if ( GL_NO_ERROR != status )
@@ -122,7 +92,7 @@ GeomMesh::render()
 			return false;
 		}
 
-		glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( int ) * this->faceCount * 3, this->indexBuffer, GL_STATIC_DRAW );
+		glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( GLuint ) * this->faceCount * 3, this->indexBuffer, GL_STATIC_DRAW );
 		status = glGetError();
 		if ( GL_NO_ERROR != status )
 		{
@@ -139,6 +109,46 @@ GeomMesh::render()
 		}
 	}
 
+	glBindBuffer( GL_ARRAY_BUFFER, this->dataVBO );
+	status = glGetError();
+	if ( GL_NO_ERROR != status )
+	{
+		cout << "GeomMesh::render: glBindBuffer GL_ARRAY_BUFFER failed: " << gluErrorString( status )  << endl;
+		return false;
+	}
+
+	glEnableVertexAttribArray( 0 );
+	status = glGetError();
+	if ( GL_NO_ERROR != status )
+	{
+		cout << "GeomMesh::render: glEnableVertexAttribArray(0) failed: " << gluErrorString( status )  << endl;
+		return false;
+	}
+
+	glEnableVertexAttribArray( 1 );
+	status = glGetError();
+	if ( GL_NO_ERROR != status )
+	{
+		cout << "GeomMesh::render: glEnableVertexAttribArray(1) failed: " << gluErrorString( status )  << endl;
+		return false;
+	}
+
+	glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, sizeof( VertexData ), BUFFER_OFFSET( 0 ) );
+	status = glGetError();
+	if ( GL_NO_ERROR != status )
+	{
+		cout << "GeomMesh::render: glVertexAttribPointer(0) failed: " << gluErrorString( status )  << endl;
+		return false;
+	}
+
+	glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, sizeof( VertexData ), BUFFER_OFFSET( 12 ) );
+	status = glGetError();
+	if ( GL_NO_ERROR != status )
+	{
+		cout << "GeomMesh::render: glVertexAttribPointer(1) failed: " << gluErrorString( status )  << endl;
+		return false;
+	}
+
 	glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, this->indexVBO );
 	status = glGetError();
 	if ( GL_NO_ERROR != status )
@@ -147,7 +157,7 @@ GeomMesh::render()
 		return false;
 	}
 
-	glDrawElements( GL_TRIANGLES, this->faceCount * 3, GL_INT, BUFFER_OFFSET( 0 ) );
+	glDrawElements( GL_TRIANGLES, this->faceCount * 3, GL_UNSIGNED_INT, BUFFER_OFFSET( 0 ) );
 	status = glGetError();
 	if ( GL_NO_ERROR != status )
 	{
@@ -160,6 +170,30 @@ GeomMesh::render()
 	if ( GL_NO_ERROR != status )
 	{
 		cout << "GeomMesh::render: glBindBuffer(0) GL_ELEMENT_ARRAY_BUFFER - render - failed: " << gluErrorString( status )  << endl;
+		return false;
+	}
+
+	glBindBuffer( GL_ARRAY_BUFFER, 0 );
+	status = glGetError();
+	if ( GL_NO_ERROR != status )
+	{
+		cout << "GeomMesh::render: glBindBuffer(0) GL_ELEMENT_ARRAY_BUFFER - render - failed: " << gluErrorString( status )  << endl;
+		return false;
+	}
+
+	glDisableVertexAttribArray( 0 );
+	status = glGetError();
+	if ( GL_NO_ERROR != status )
+	{
+		cout << "GeomMesh::render: glDisableVertexAttribArray(1) failed: " << gluErrorString( status )  << endl;
+		return false;
+	}
+
+	glDisableVertexAttribArray( 1 );
+	status = glGetError();
+	if ( GL_NO_ERROR != status )
+	{
+		cout << "GeomMesh::render: glDisableVertexAttribArray(1) failed: " << gluErrorString( status )  << endl;
 		return false;
 	}
 
--- src/SubSystems/Graphics/ZazenGraphics/Material/Program.cpp
+++ src/SubSystems/Graphics/ZazenGraphics/Material/Program.cpp
@@ -120,12 +120,22 @@ bool
 Program::bindUniformBlock( UniformBlock* block )
 {
 	GLint status;
+	GLint uniformBlockSize;
 
 	GLuint index = this->getUniformBlockIndex( block->getName() );
 	if ( GL_INVALID_INDEX == index )
 		return false;
 
-	glUniformBlockBinding( this->programObject, index, block->getID() );
+	glGetActiveUniformBlockiv( this->programObject, index, GL_UNIFORM_BLOCK_DATA_SIZE, &uniformBlockSize);
+
+	// create the UBO
+	if ( false == block->updateData( 0, uniformBlockSize ) )
+		return false;
+
+	if ( false == block->bind( 0 ) )
+		return false;
+
+	glUniformBlockBinding( this->programObject, index, 0 );
 	status = glGetError();
 	if ( GL_NO_ERROR != status )
 	{
