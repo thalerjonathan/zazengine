d197fd2c0aed2f04090969823fdb2e55a1dcac72
thaler.jonathan <thaler.jonathan@bb54a4e1-d722-1b89-d25c-b3a6cc25c7a0>
2011-02-11 17:51:51 +0000

    next step toward mrt

--- trunk/media/graphics/dr/stages/geom/geomFrag.glsl
+++ trunk/media/graphics/dr/stages/geom/geomFrag.glsl
@@ -1,27 +1,17 @@
 #version 330 core
 
 in vec4 ex_normal;
-in vec4 ex_color;
-in vec4 ex_shadowCoord;
 
 out vec4 out_diffuse;
 out vec4 out_normal;
 out vec4 out_depth;
 out vec4 out_generic;
 
-uniform sampler2DShadow ShadowMap;
-
-float lookup( float offsetX, float offsetY )
-{
-	float depth = textureProj( ShadowMap, ex_shadowCoord + vec4( offsetX, offsetY, 0.0, 0.005 ) );
-	return depth != 1.0 ? 0.75 : 1.0; 
-}
-
 void main()
 {
-	out_diffuse = ex_color * lookup( 0.0, 0.0 );
+	out_diffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
 	out_normal = ex_normal;
 	out_depth = vec4( 0.0, 0.0, 1.0, 1.0 );
-	out_generic = vec4( 1.0, 1.0, 0, 1.0 );
+	out_generic = vec4( 1.0, 0.0, 0, 1.0 );
 
 }
