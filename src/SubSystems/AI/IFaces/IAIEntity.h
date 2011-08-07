53e923d1e1e82f902447cf05394ec0c8beda3ff9
thaler.jonathan@gmail.com <thaler.jonathan@gmail.com@bb54a4e1-d722-1b89-d25c-b3a6cc25c7a0>
2011-08-07 11:15:58 +0000

    ifaces from subsystems

---
+++ src/SubSystems/AI/IFaces/IAIEntity.h
@@ -0,0 +1,21 @@
+/*
+ * IAIEntity.h
+ *
+ *  Created on: 15.07.2010
+ *      Author: joni
+ */
+
+#ifndef IAIENTITY_H_
+#define IAIENTITY_H_
+
+#include "ISubSystemEntity.h"
+
+class IAIEntity : public ISubSystemEntity
+{
+	public:
+		IAIEntity( IGameObject* p ) : ISubSystemEntity( p ) {};
+		virtual ~IAIEntity() {};
+
+};
+
+#endif /* IAIENTITY_H_ */
