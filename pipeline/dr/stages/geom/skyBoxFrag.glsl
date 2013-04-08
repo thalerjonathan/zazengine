520375b598f81802e7cfdb7311bc5d1698ff0c44
thaler.jonathan@gmail.com <thaler.jonathan@gmail.com@bb54a4e1-d722-1b89-d25c-b3a6cc25c7a0>
2013-04-08 22:21:40 +0000

    blub

---
+++ pipeline/dr/stages/geom/skyBoxFrag.glsl
@@ -0,0 +1,16 @@
+#version 330 core
+
+in vec3 ex_texCoord;
+
+uniform samplerCube SkyBoxCubeMap;
+
+// OPTIMIZE: remove bindfraglocation in init
+layout( location = 0 ) out vec4 out_diffuse;
+
+void main()
+{
+	// sky-box does diffuse-color only
+	out_diffuse.rgb = texture( SkyBoxCubeMap, ex_texCoord ).rgb;
+	// store special material-type for sky-box
+	out_diffuse.a = 255;
+}
