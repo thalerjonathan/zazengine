a2301b801f4b2865057807e61981d56d1607664b
thaler.jonathan@gmail.com <thaler.jonathan@gmail.com@bb54a4e1-d722-1b89-d25c-b3a6cc25c7a0>
2013-04-30 03:37:24 +0000

    updated todos, refined transparency shaders, but still not working properly

--- trunk/pipeline/dr/stages/transparency/transpFrag.glsl
+++ trunk/pipeline/dr/stages/transparency/transpFrag.glsl
@@ -5,6 +5,7 @@ uniform sampler2D NormalTexture;
 
 uniform sampler2D Background;
 
+in vec4 ex_pos;
 in vec4 ex_normal;
 in vec2 ex_texCoord;
 
@@ -14,15 +15,15 @@ const float constantAlpha = 0.5;
 
 void main()
 {
-    vec3 normal = vec3( texture( NormalTexture, ex_texCoord ) );
-    vec2 texCoord = ex_texCoord.xy + normal.xy;
+	vec4 bumpTex = 2.0 * texture( NormalTexture, ex_texCoord ) - 1.0;
 
-    vec3 bgColor = vec3( texture( Background, texCoord ) );
-    vec4 diffuseColor = texture( DiffuseTexture, texCoord );
+	vec2 newTexCoord = ( ex_pos.xy / ex_pos.w ) + bumpTex.xy;
+
+	vec4 bgColor = texture( Background, newTexCoord );
+    vec4 diffuseColor = texture( DiffuseTexture, ex_texCoord );
 
 	diffuseColor.a = constantAlpha;
 
-    // when using the diffuse-color texture as alpha source:
-    out_color.rgb = diffuseColor.rgb * diffuseColor.a + bgColor.rgb * ( 1.0 - diffuseColor.a );
+	out_color.rgb = diffuseColor.rgb * diffuseColor.a + bgColor.rgb * ( 1.0 - diffuseColor.a );
     out_color.a = 0.0;
 }
