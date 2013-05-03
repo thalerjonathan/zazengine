88e657211542d81bd1fda563b36d6bf3b69b9ab0
thaler.jonathan@gmail.com <thaler.jonathan@gmail.com@bb54a4e1-d722-1b89-d25c-b3a6cc25c7a0>
2013-05-03 16:54:08 +0000

    restructured transparency shader, but depth-lookup not working, need to copy it!

--- trunk/pipeline/dr/stages/transparency/transpFrag.glsl
+++ trunk/pipeline/dr/stages/transparency/transpFrag.glsl
@@ -4,6 +4,7 @@ uniform sampler2D DiffuseTexture;
 uniform sampler2D NormalTexture;
 
 uniform sampler2D Background;
+uniform sampler2D BackgroundDepth;
 
 in vec4 ex_pos;
 in vec4 ex_normal;
@@ -19,21 +20,32 @@ layout( shared ) uniform CameraUniforms
 	vec4 rectangle;
 } Camera;
 
-const float constantAlpha = 0.001;
+const float blendingFactor = 0.5;
+const float refractFactor = 0.05;
 
 void main()
 {
-	vec4 bumpTex = 2.0 * texture( NormalTexture, ex_texCoord ) - 1.0;
+	vec4 refractNormal = 2.0 * texture( NormalTexture, ex_texCoord ) - 1.0;
 
-	vec2 screenCoord = vec2( gl_FragCoord.x / Camera.rectangle.x, gl_FragCoord.y / Camera.rectangle.y );
+	vec2 screenTexCoord = vec2( gl_FragCoord.x / Camera.rectangle.x, gl_FragCoord.y / Camera.rectangle.y );
+	vec2 refractTexCoord = screenTexCoord + refractNormal.xy * refractFactor;
 
-	vec2 newTexCoord = screenCoord + bumpTex.xy;
+	vec4 bgColorRefract = texture( Background, refractTexCoord );
+	vec4 bgColorScreen = texture( Background, screenTexCoord );
 
-	vec4 bgColor = texture( Background, newTexCoord );
-    vec4 diffuseColor = texture( DiffuseTexture, ex_texCoord );
+    vec4 materialColor = texture( DiffuseTexture, ex_texCoord );
+	
+	vec3 refractFragDepth = texture( BackgroundDepth, refractTexCoord );
 
-	diffuseColor.a = constantAlpha;
+	if ( refractFragDepth.r < gl_FragDepth )
+	{
+		out_color.rgb = materialColor.rgb * blendingFactor + bgColorScreen.rgb * ( 1.0 - blendingFactor );
+	}
+	else
+	{
+		out_color.rgb = materialColor.rgb * blendingFactor + bgColorRefract.rgb * ( 1.0 - blendingFactor );
+	}
 
-	out_color.rgb = diffuseColor.rgb * diffuseColor.a + bgColor.rgb * ( 1.0 - diffuseColor.a );
+	out_color.rgb = texture( BackgroundDepth, ex_texCoord );
     out_color.a = 0.0;
 }
